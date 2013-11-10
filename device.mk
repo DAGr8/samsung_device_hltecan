d67cd821810269bd0acc89b6a9abafdb0743463f
DAGr8 <dagr82003@yahoo.com>
2013-11-10 11:13:34 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jfltetmo/jfltetmo-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/hte/hlte-vendor.mk)
 
 ## device overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/hltecan/overlay
